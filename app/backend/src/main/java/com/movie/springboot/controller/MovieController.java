09a6e6883c556c463ee542a25463c31cdd3f0d04
Burak Gundogdu <burak.gundogdu0131@gmail.com>
2020-08-16 16:42:19 +0300

    bg adds swagger ui

--- app/backend/src/main/java/com/movie/springboot/controller/MovieController.java
+++ app/backend/src/main/java/com/movie/springboot/controller/MovieController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -15,6 +16,7 @@ import com.movie.springboot.models.Movie;
 import com.movie.springboot.models.MovieDetail;
 
 @RestController
+@RequestMapping("api")
 public class MovieController {
 
 	@Autowired
