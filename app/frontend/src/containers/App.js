b36ca9164b890af8426d17d1a4ab87dbd35d1b73
Zeynep Bekem <zbekem@gmail.com>
2020-08-16 22:39:53 +0300

    Update: App.js

--- app/frontend/src/containers/App.js
+++ app/frontend/src/containers/App.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import Navbar from '../components/layout/Navbar';
-import SignIn from '../containers/SignIn';
-import SignUp from '../containers/SignUp';
-import MovieDetail from '../containers/MovieDetail';
-import Movies from './Movies';
+import React from "react";
+import Navbar from "../components/layout/Navbar";
+import SignIn from "../containers/SignIn";
+import SignUp from "../containers/SignUp";
+import MovieDetail from "../containers/MovieDetail";
+import Movies from "./Movies";
 
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-} from "react-router-dom";
+import MovieContentPage from "../containers/MovieContent/MovieContent";
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Switch>
-        <Route path='/signin' component={SignIn} />
-        <Route path='/signup' component={SignUp} />
-        <Route path='/movies' component={Movies} />
+        <Route path="/signin" component={SignIn} />
+        <Route path="/signup" component={SignUp} />
+        <Route path="/movies" component={Movies} />
         <Route path="/movie/:id" component={MovieDetail} />
+        <Route path="/moviesprofile" component={MovieContentPage} />
       </Switch>
     </Router>
   );
