5545746ce261ab57c57ec9acd16e051995086ba5
gozetfiliz <filiz.gozet9@gmail.com>
2020-08-17 01:34:26 +0300

    Remove: MovieContent

--- app/frontend/src/containers/App.js
+++ app/frontend/src/containers/App.js
@@ -5,8 +5,6 @@ import SignUp from "../containers/SignUp";
 import MovieDetail from "../containers/MovieDetail";
 import Movies from "./Movies";
 
-import MovieContentPage from "../containers/MovieContent/MovieContent";
-
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
@@ -18,7 +16,6 @@ function App() {
         <Route path="/signup" component={SignUp} />
         <Route path="/movies" component={Movies} />
         <Route path="/movie/:id" component={MovieDetail} />
-        <Route path="/moviesprofile" component={MovieContentPage} />
       </Switch>
     </Router>
   );
